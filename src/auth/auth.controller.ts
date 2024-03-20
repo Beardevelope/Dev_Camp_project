e154cfaa7ca2566fdf63bb761f6f59b2eae5eaff
Beardevelope <mck84835182@gmail.com>
2024-03-20 23:27:20 +0900

    NEW : auth.controller endpoint : signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,34 +9,22 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from './services/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,
+    private readonly userService: UserService,) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.authService.create(createUserDto);
-  }
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.createUser(createUserDto)
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+    return {
+      id: user.id,
+      name:user.name,
+      email: user.email,
+      phone:user.phone
+    }
   }
 }
