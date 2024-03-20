91619460d310898b00719bf6cb4227da01b82a80
Beardevelope <mck84835182@gmail.com>
2024-03-20 23:23:47 +0900

    FEATURE : USER SERVICE - CREATEUSER

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# dev_camp
-=======
 # Dev_Camp_project
->>>>>>> 2717dd9b0cdb59612044f65dbbff3ad72a4b5ced
--- src/auth/services/user.service.ts
+++ src/auth/services/user.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { UpdateAuthDto } from '../dto/update-auth.dto';
+import { UserRepository } from '../repositories/user.repository';
+import { User } from '../entities/user.entity';
+import * as argon2 from 'argon2';
 
 @Injectable()
-export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = await this.getUserByEmail(createUserDto.email);
+    if (user) {
+      throw new BadRequestException(
+        `${createUserDto.email} already exists`,
+        'user',
+      );
+    }
+    const hashedPassword = await argon2.hash(createUserDto.password);
+    return this.userRepository.createUser(createUserDto, hashedPassword);
+  }
+
+  getUserByEmail(email: string) {
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
   }
 
   findAll() {
@@ -14,10 +34,6 @@ export class AuthService {
 
   findOne(id: number) {
     return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
   }
 
   remove(id: number) {
